53370b8bb21cd43966ae2b98686ab80cddcc4bde
sharp.coding <sharp.coding@gmail.com>
2017-04-24 21:50:49 +0200

    Adding new "Array with random-generated mutations" test

--- test/trainPredict.js
+++ test/trainPredict.js
@@ -8,4 +8,35 @@ describe("Train/predict works", function () {
         expect(predicted[2]).not.toBe(0);
         expect(predicted[3]).not.toBe(0);
     });
+    it("Array with random-generated mutations", function () {
+        var samplesLength = 100;
+        var probabilitiyOfMutation = 10;
+        var valueChangeRange = { minLow: -3, maxHigh: 3 };
+        var mutate = function () { return _.random(0, 100) <= probabilitiyOfMutation; };
+        var changeCurrentValue = function () { return _.random(1, 100) < 10; };
+        var samples = [];
+        var currentValue = 5000; //_.random(1, 50000);
+        for (var i = 0; i < samplesLength; i++) {
+            samples.push({
+                trainedValue: currentValue + _.random(valueChangeRange.minLow, valueChangeRange.maxHigh)
+            });
+        }
+        _.each(samples, function (el) {
+            el.mutated = mutate();
+            el.testedValue = el.mutated ? el.trainedValue * 10000 : el.trainedValue;
+        });
+        var model = TS.train(_.map(samples, function (el) { return el.trainedValue; }));
+        var predictionVector = TS.predict(model)(_.map(samples, function (el) { return el.testedValue; }));
+        console.log(predictionVector);
+        for (var i = 0; i < predictionVector.length; i++) {
+            samples[i].predictionValue = predictionVector[i];
+        }
+        console.log('mutations', _.filter(samples, function (el) { return el.mutated; }));
+        _.each(samples, function (el) {
+            if (el.mutated)
+                expect(el.predictionValue).toBe(0);
+            else
+                expect(el.predictionValue).not.toBe(0);
+        });
+    });
 });
--- test/trainPredict.ts
+++ test/trainPredict.ts
@@ -8,4 +8,40 @@ describe("Train/predict works", () => {
     expect(predicted[2]).not.toBe(0);
     expect(predicted[3]).not.toBe(0);
   });
+
+  it("Array with random-generated mutations", () => {
+    const samplesLength = 100;
+    const probabilitiyOfMutation = 10;
+    type Sample = { 
+      trainedValue: number;
+      testedValue: number;
+      predictionValue: number;
+      mutated: boolean;
+    }
+    let valueChangeRange = { minLow: -3, maxHigh: 3 };
+    let mutate = ():boolean => _.random(0, 100) <= probabilitiyOfMutation;
+    let changeCurrentValue = ():boolean => _.random(1, 100) < 10;
+    let samples: Sample[] = [];
+    let currentValue = 5000;
+    for (let i=0; i < samplesLength; i++) {
+      samples.push(<Sample>{
+        trainedValue: currentValue + _.random(valueChangeRange.minLow, valueChangeRange.maxHigh)
+      });
+    }
+    _.each(samples, el => {
+      el.mutated = mutate(); 
+      el.testedValue = el.mutated ? el.trainedValue*10000 : el.trainedValue;
+    });
+    let model = TS.train(_.map(samples, el => el.trainedValue));
+    let predictionVector = TS.predict(model)(_.map(samples, el => el.testedValue));
+    for (let i=0; i < predictionVector.length; i++) {
+      samples[i].predictionValue = predictionVector[i];
+    }
+    _.each(samples, el => {
+      if (el.mutated)
+        expect(el.predictionValue).toBe(0);
+      else
+        expect(el.predictionValue).not.toBe(0);
+    })
+  });
 });
